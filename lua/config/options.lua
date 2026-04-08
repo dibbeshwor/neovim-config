847144b7b2b5445d85575508fdd08e2aec1f7d05
Dibbeshwor Acharya <dibbeshwor@gmail.com>
2026-04-08 12:23:42 +0545

    Update options.

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -9,11 +9,36 @@ vim.opt.encoding = "utf-8"
 vim.opt.fileencoding = "utf-8"
 
 vim.opt.number = true
-
+vim.opt.relativenumber = true
+vim.opt.cursorline = true
 vim.opt.title = true
-vim.opt.autoindent = true
-vim.opt.smartindent = true
-vim.opt.hlsearch = true
+vim.opt.wrap = false
+vim.opt.signcolumn = "yes"
 
-vim.opt.scrolloff = 12
+vim.opt.expandtab = true
+vim.opt.shiftwidth = 2
+vim.opt.tabstop = 2
+vim.opt.smartindent = true
+
+-- Python override
+vim.api.nvim_create_autocmd("FileType", {
+  pattern = "python",
+  callback = function()
+    vim.opt_local.shiftwidth = 4
+    vim.opt_local.tabstop = 4
+    vim.opt_local.expandtab = true
+  end,
+})
+
+vim.opt.smartcase = true
+vim.opt.hlsearch = true
+vim.opt.incsearch = true
+
+vim.opt.undofile = true
+
+vim.opt.scrolloff = 8
 vim.opt.ignorecase = true
+
+vim.opt.updatetime = 250
+
+vim.opt.clipboard = "unnamedplus"
